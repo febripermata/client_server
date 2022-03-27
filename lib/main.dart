9f4813483a5b565974e0d0944dc1243cd059bfa5
Febri Permata <emiliana.febri@gmail.com>
2022-03-27 19:38:40 +0700

    tes

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:client_server/model/article.dart';
+import 'package:client_server/ui/article_detail_page.dart';
+import 'package:client_server/ui/article_list_page.dart';
+import 'package:client_server/ui/article_web_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => ArticleListPage(),
+        'article_web': (context) =>
+            ArticleWebView(
+              // url: ModalRoute.of(context)!.settings.arguments as String
+            ),
+        '/article_detail': (context) =>
+            ArticleDetailPage(
+              // ModalRoute.of(context)!.settings.arguments as Article
+            ),
+      },
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
